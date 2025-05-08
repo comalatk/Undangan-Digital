008f33fc398cb9a42d521459a2ee6106c280abc7
comalatk <149460863+comalatk@users.noreply.github.com>
2025-05-08 20:34:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// Fungsi untuk menutup popup
-function tutupPopup() {
-  const popup = document.getElementById("popup");
-  popup.style.display = "none";
-}
-
 // Ambil parameter nama dari URL
 function getNamaTamu() {
   const params = new URLSearchParams(window.location.search);
-  const nama = params.get("nama");
+  const nama = params.get("to");
 
   if (nama) {
-    document.getElementById("nama-tamu").textContent = decodeURIComponent(nama);
+    document.getElementById("nama-tamu").textContent = decodeURIComponent(nama.replace(/\+/g, ' '));
   }
 }
 
-// Jalankan saat halaman selesai dimuat
+// Tampilkan konten undangan saat tombol diklik
+document.getElementById("buka-undangan").addEventListener("click", function() {
+  document.getElementById("hero").classList.add("hidden");
+  document.getElementById("konten-undangan").classList.remove("hidden");
+});
+
+// Inisialisasi
 window.onload = function () {
   getNamaTamu();
-  document.getElementById("popup").style.display = "flex";
 };
