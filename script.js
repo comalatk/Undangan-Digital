c52ba294519e045e042c349d626ed22f42f3691f
comalatk <149460863+comalatk@users.noreply.github.com>
2025-05-08 15:43:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get("to");
+if (nama) {
+  document.getElementById("namaTamu").innerText = decodeURIComponent(nama);
+}
